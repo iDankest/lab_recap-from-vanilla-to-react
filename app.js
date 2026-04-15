08053fd09fc3b7a8dcdc22c5b578bc378878c16b
Kilian <dankestmirror@hotmail.es>
2026-04-15 18:48:31 +0100

    Me peta el Cerebro

--- app.js
+++ app.js
@@ -52,3 +52,4 @@ btn.addEventListener("click", () => {
         renderUsers();
     });
 });
+
--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -6,6 +6,21 @@ function App() {
  const [users, setUsers] = useState([]);
  const [skip, setSkip] = useState(0);
  const [loading, setLoading] = useState(false);
+ const limit = 10;
+
+ const fetchUser = async () => {
+    setLoading(true);
+      const API = `https://dummyjson.com/users?limit=${limit}&skip=${skip}`;
+    try {
+       const response = await fetch(API);
+        const jsonData = await response.json();
+    } catch (error) {
+      
+    } finally {
+      setLoading(false);
+    }
+ }
+
 
   return (
     <>
